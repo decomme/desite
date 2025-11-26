9c3fabd7bbf594a3f0fd33cd61a3b625cbddd1fa
Denizify <1996gursoydeniz@gmail.com>
2025-11-26 10:44:16 +0300

    burger_btn_fixing
    Burger butondaki ufak tefek hatalar giderildi

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -15,23 +15,29 @@ $(function () {
 
         // Desktop için: body'ye menu-collapsed class'ı
         $body.toggleClass('menu-collapsed');
+
+        // Butonun pozisyonu için class
+        $toggler.toggleClass('is-open');
     });
 
     // 2) Menü linkine tıklanınca menüyü kapat
     $navLinks.on('click', function () {
         $header.removeClass('show');          // mobil
         $body.addClass('menu-collapsed');     // desktop: kapalı hale getir
+        $toggler.removeClass('is-open');      // butonu da kapalı moda al
     });
 
     // 3) Header ve butonun dışına tıklayınca menüyü kapat
     $(document).on('click', function (e) {
         var $target = $(e.target);
 
-        if (!$target.closest('.tm-header').length &&
-            !$target.closest('.navbar-toggler').length) {
-
+        if (
+            !$target.closest('.tm-header').length &&
+            !$target.closest('.navbar-toggler').length
+        ) {
             $header.removeClass('show');      // mobil
             $body.addClass('menu-collapsed'); // desktop
+            $toggler.removeClass('is-open');  // buton kapalı
         }
     });
 });
